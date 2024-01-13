cf0c2b7978ab6a8399111d6c34145ceedf322c81
Data-Nexus <derek.m.meyer@gmail.com>
2024-01-13 15:07:58 -0500

    fix templates & networks

--- script/utils/networks.ts
+++ script/utils/networks.ts
@@ -10,36 +10,36 @@ export interface NetworkConfigs {
 
 export const networks : NetworkConfigs=  {
     "arbitrum-one": {
-        studioEndpoint: "univ3-arbitrum",
-        hostedEndpoint: "univ3-arbitrum"
+        studioEndpoint: "univ3-tokens-arbitrum",
+        hostedEndpoint: "univ3-tokens-arbitrum"
     },
     "ethereum": {
-        studioEndpoint: "univ3-ethereum",
-        hostedEndpoint: "gundamdweeb/univ3-token-pricing-main"
+        studioEndpoint: "univ3-tokens-ethereum",
+        hostedEndpoint: "univ3-tokens-ethereum"
     },
     "avax": {
-        studioEndpoint: "univ3-avax",
-        hostedEndpoint: "univ3-avax"
+        studioEndpoint: "univ3-tokens-avax",
+        hostedEndpoint: "univ3-tokens-avax"
     },
     "base": {
-        studioEndpoint: "univ3-base",
-        hostedEndpoint: "univ3-base"
+        studioEndpoint: "univ3-tokens-base",
+        hostedEndpoint: "univ3-tokens-base"
     },
     "celo": {
-        studioEndpoint: "univ3-celo",
-        hostedEndpoint: "univ3-celo"
+        studioEndpoint: "univ3-tokens-celo",
+        hostedEndpoint: "univ3-tokens-celo"
     },
     "polygon": {
-        studioEndpoint: "univ3-polygon",
-        hostedEndpoint: "univ3-polygon"
+        studioEndpoint: "univ3-tokens-polygon",
+        hostedEndpoint: "univ3-tokens-polygon"
     },
     "optimism": {
-        studioEndpoint: "univ3-optimism",
-        hostedEndpoint: "univ3-optimism"
+        studioEndpoint: "univ3-tokens-optimism",
+        hostedEndpoint: "univ3-tokens-optimism"
     },
     "bsc": {
-        studioEndpoint: "univ3-bsc",
-        hostedEndpoint: "univ3-bsc"
+        studioEndpoint: "univ3-tokens-bsc",
+        hostedEndpoint: "univ3-tokens-bsc"
     },
 } 
     
